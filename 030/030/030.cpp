7da5fc429ef60b2e79e71114e3deb8542f577828
JamAlex17 <fm928@yandex.ru>
2016-06-18 20:01:24 +0300

    Added 030 Solution

---
+++ 030/030/030.cpp
@@ -0,0 +1,37 @@
+// 030.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+#define MAX_NUMBER 9 * 9 * 9 * 9 * 9 * 9 * 5
+
+using namespace std;
+
+int main()
+{
+	int total_all = 0;
+
+	for (int i = 2; i < MAX_NUMBER; i++)
+	{
+		int total = 0;
+		int n = i;
+
+		while (true)
+		{
+			total += (n % 10) * (n % 10)* (n % 10) * (n % 10) * (n % 10);
+			n /= 10;
+			if (n == 0) break;
+		}
+
+		if (i == total)
+		{
+			total_all += i;
+		}
+	}
+
+	cout << "total = " << total_all << endl;
+	getchar();
+    return 0;
+}
+
