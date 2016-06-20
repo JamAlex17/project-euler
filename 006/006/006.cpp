2d369ced9bfc551da75fae565f48bd2e5036df4b
JamAlex17 <fm928@yandex.ru>
2016-06-20 22:57:22 +0300

    add small refactoring

--- 006/006/006.cpp
+++ 006/006/006.cpp
@@ -45,15 +45,8 @@ private:
 int problem_006()
 {
 	int result = 0;
-	int sum_squares = 0;
-
-	for (int i = 1; i <= NUMBER; i++)
-	{
-		sum_squares += i*i;
-	}
-
+	int sum_squares = NUMBER*(NUMBER+1)*(2*NUMBER+1)/6;	
 	int sum_natural = (1 + NUMBER)*NUMBER / 2;
-
 	result = sum_natural*sum_natural - sum_squares;
 
 	return result;
